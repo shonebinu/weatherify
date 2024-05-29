429ba7c29ea110507104d401804353171fe0ec6e
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-05-29 20:40:32 +0530

    implement frontend and backend connection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,175 @@
 import "./styles.css";
+import { parseISO, format } from "date-fns";
 import WeatherAPI from "./weatherAPI";
+
+const unit = document.querySelector(".unit");
+const location = document.querySelector(".location");
+
+if (!localStorage.getItem("location")) {
+	const ipData = await WeatherAPI.getIP();
+	const currLocation = `${ipData.region}, ${ipData.country_name}`;
+	ipData.region + localStorage.setItem("location", currLocation);
+}
+
+if (!localStorage.getItem("isCelsius")) {
+	localStorage.setItem("isCelsius", true);
+}
+
+updateUI();
+
+unit.addEventListener("click", () => {
+	const isCelsius = localStorage.getItem("isCelsius") === "true";
+	localStorage.setItem("isCelsius", isCelsius ? false : true);
+	updateUI();
+});
+
+location.querySelector("button").addEventListener("click", async () => {
+	const locationInput = document.querySelector(".location > input").value;
+	const trueLocation = await WeatherAPI.getForeCastWeather(locationInput);
+	if (trueLocation.location) {
+		localStorage.setItem(
+			"location",
+			`${trueLocation.location.name}, ${trueLocation.location.region}`,
+		);
+	}
+	updateUI();
+});
+
+location.querySelector("input").addEventListener("keypress", (e) => {
+	if (e.key === "Enter") {
+		e.preventDefault();
+		location.querySelector("button").click();
+	}
+});
+
+function formatDate(dateString) {
+	const date = parseISO(dateString);
+	const dayFormatString = "EEEE";
+	const dateFormatString = "dd MMMM yyyy";
+	const dayOfWeek = format(date, dayFormatString);
+	const formattedDate = format(date, dateFormatString);
+	return `<strong>${dayOfWeek},</strong> ${formattedDate}`;
+}
+
+function formatHour(dateString) {
+	const date = parseISO(dateString);
+	const hour = format(date, "h");
+	const period = format(date, "a");
+	return `${hour} ${period.toUpperCase()}`;
+}
+
+async function updateUI() {
+	const location = localStorage.getItem("location");
+	const isCelsius = localStorage.getItem("isCelsius") === "true";
+
+	updateTopBar(location, isCelsius); // since the data is already available, update first
+
+	const historyData = await WeatherAPI.getHistoryWeather(location, 1);
+	const forecastData = await WeatherAPI.getForeCastWeather(location, 3);
+
+	updateTodayBar(forecastData, isCelsius);
+	updateHourlyBar(forecastData.forecast.forecastday[0].hour);
+
+	updateWeekBar(
+		[
+			historyData.forecast.forecastday[0],
+			forecastData.forecast.forecastday[1],
+			forecastData.forecast.forecastday[2],
+		],
+		isCelsius,
+	);
+}
+
+function updateTopBar(location, celsius) {
+	const section = document.querySelector(".top-bar");
+	const unitSpan = section.querySelector(".unit > span");
+	const locationInput = section.querySelector(".location > input");
+
+	unitSpan.textContent = celsius ? "C" : "F";
+	locationInput.value = location;
+}
+
+function updateTodayBar(data, isCelsius) {
+	const section = document.querySelector(".main-container");
+	const currStateImg = section.querySelector(".state");
+	const currTemp = section.querySelector(".temp > span");
+	const brief = section.querySelector(".today-highlight .brief");
+	const feelsLike = section.querySelector(".feels-like > span");
+	const date = section.querySelector(".date");
+	const minTemp = section.querySelector(".min-temp");
+	const maxTemp = section.querySelector(".max-temp");
+	const rainChance = section.querySelector(".rain-chance");
+	const humidity = section.querySelector(".humidity");
+	const sunrise = section.querySelector(".sunrise");
+	const sunset = section.querySelector(".sunset");
+
+	currStateImg.src = data.current.condition.icon;
+	currTemp.textContent = isCelsius ? data.current.temp_c : data.current.temp_f;
+	brief.textContent = data.current.condition.text;
+	feelsLike.textContent = isCelsius
+		? data.current.feelslike_c
+		: data.current.feelslike_f;
+	date.innerHTML = formatDate(data.forecast.forecastday[0].date);
+	minTemp.textContent = isCelsius
+		? data.forecast.forecastday[0].day.mintemp_c
+		: data.forecast.forecastday[0].day.mintemp_f;
+	maxTemp.textContent = isCelsius
+		? data.forecast.forecastday[0].day.maxtemp_c
+		: data.forecast.forecastday[0].day.maxtemp_f;
+	rainChance.textContent =
+		data.forecast.forecastday[0].day.daily_chance_of_rain;
+	humidity.textContent = data.current.humidity;
+	sunrise.textContent = data.forecast.forecastday[0].astro.sunrise;
+	sunset.textContent = data.forecast.forecastday[0].astro.sunset;
+}
+
+function updateHourlyBar(data) {
+	const section = document.querySelector(".hourly");
+	section.innerHTML = "";
+
+	for (let j = 0, i = 3; j < 6; j++, i = (i + 4) % 24) {
+		const time = formatHour(data[i].time);
+		const img = data[i].condition.icon;
+		const condition = data[i].condition.text;
+
+		const hourDiv = document.createElement("div");
+		hourDiv.classList.add("hour");
+
+		hourDiv.innerHTML = `
+            <p>${time}</p>
+            <img src="${img}">
+            <p>${condition}</p>
+        `;
+
+		section.appendChild(hourDiv);
+	}
+}
+
+function updateWeekBar(data, isCelsius) {
+	const section = document.querySelector(".week");
+	const daily = section.querySelectorAll(".daily");
+
+	updateDayCard(data[0], isCelsius, daily[0]);
+	updateDayCard(data[1], isCelsius, daily[1]);
+	updateDayCard(data[2], isCelsius, daily[2]);
+}
+
+function updateDayCard(data, isCelsius, elem) {
+	const img = elem.querySelector("img");
+	const temp = elem.querySelector(".temp > span");
+	const brief = elem.querySelector(".brief");
+	const date = elem.querySelector(".date");
+	const rainChance = elem.querySelector(".rain-chance");
+	const humidity = elem.querySelector(".humidity");
+	const sunrise = elem.querySelector(".sunrise");
+	const sunset = elem.querySelector(".sunset");
+
+	img.src = data.day.condition.icon;
+	temp.textContent = isCelsius ? data.day.avgtemp_c : data.day.avgtemp_f;
+	brief.textContent = data.day.condition.text;
+	date.innerHTML = formatDate(data.date);
+	rainChance.textContent = data.day.daily_chance_of_rain;
+	humidity.textContent = data.day.avghumidity;
+	sunrise.textContent = data.astro.sunrise;
+	sunset.textContent = data.astro.sunset;
+}
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -7,9 +7,6 @@ const WeatherAPI = (() => {
 			const response = await fetch(url, {
 				mode: "cors",
 			});
-			if (!response.ok) {
-				throw new Error("Network response was not ok");
-			}
 			const data = await response.json();
 			return data;
 		} catch (err) {
